7c35a6ea4e8eca80b32582a9f875718fde9d2a53
Ahmed Yousef <AYousef@steelnetwork.com>
2025-12-20 06:09:04 +0200

    Seed roles, permissions, and admin user on startup
    Added DataSeeder to initialize default roles, assign all permissions to the Admin role, and create a default admin user if none exist. Enhanced Permissions.cs with a Users group and a method to enumerate all permissions. Integrated seeding logic into Program.cs to automate initial data setup during application startup.

---
+++ src/Identity.Infrastructure/Seeders/DataSeeder.cs
@@ -0,0 +1,75 @@
+﻿using Identity.Application.Abstractions;
+using Identity.Domain.Entity;
+using Identity.Domain.ValueObjects;
+using Identity.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel.Constants;
+using SharedKernel.Exceptions;
+
+
+namespace Identity.Infrastructure.Seeders;
+
+public class DataSeeder
+{
+
+    public static async Task SeedAsync(IdentityDbContext context, IPasswordHasher passwordHasher)
+    {
+
+        #region Roles
+
+        if (!context.Roles.Any())
+        {
+            var admin = Role.Create("Admin", "Administrator role with full permissions");
+            var user = Role.Create("User", "Standard user role with limited permissions");
+
+            context.Roles.AddRange(admin, user);
+            await context.SaveChangesAsync();
+
+        }
+
+        #endregion
+
+        #region Permissions for Admin Role
+
+        var adminRole = await context.Roles
+                             .Include(r => r.Permissions)
+                             .FirstOrDefaultAsync(r => r.Name == "Admin") 
+                         ?? throw new DomainException("Admin role not found.");
+
+
+        var allPermissions = Permissions.GetAllPermissions();
+
+        foreach (var permission in allPermissions)
+        {
+            adminRole.AddPermission(permission);
+        }
+
+        await context.SaveChangesAsync();
+
+
+        #endregion
+
+        #region Default Admin User
+
+        if (!context.Users.Any())
+        {
+            var passwordHash = passwordHasher.Hash("Admin123");
+
+            var adminUser = User.Create(
+
+                 "Ahmed",
+                 "Yousef",
+                 Email.Create("admin@modularmonolith.com"),
+                 passwordHash
+            );
+
+            adminUser.AssignRole(adminRole);
+
+            context.Users.Add(adminUser);
+
+            await context.SaveChangesAsync();
+        }
+
+        #endregion
+    }
+}
